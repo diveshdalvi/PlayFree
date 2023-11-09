af94fe16f52a8713f05dcb436a325e07cb9bd805
diveshdalvi <dalvidivesh1@gmail.com>
2023-11-09 14:31:03 +0530

    done with project

--- src/Components/GameContainer.jsx
+++ src/Components/GameContainer.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 const GameList = ({games}) => {
   
   return (
     <div className='flex flex-wrap'>
         {games.map((game) => (
-            <div className=' bg-[#191717]  flex flex-col  border-white rounded-lg m-5 border-2 sm:w-1/6 p-4 w-full'>
+            <div className=' bg-[#191717]  flex flex-col  border-white rounded-lg m-5 border-2 sm:w-1/6 p-4 w-full sm:hover:scale-105 transition-transform'>
             <img src={game.thumbnail} alt="img" className='object-cover w-96 h-48'/>
             <div className='mt-3 '>
-              <div className=' text-center text-2xl  cursor-pointer mb-2'><a href= {game.game_url}>{game.title}</a></div>
+              <div className=' text-center text-2xl  cursor-pointer mb-2 '><a href= {game.game_url}>{game.title }</a></div>
 
             <p className='text-sm text-gray-400 mb-2'>{game.short_description}</p>
             <div className='mb-2 text-md'>
